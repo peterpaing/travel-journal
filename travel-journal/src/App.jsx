d234b67bd7f20bb86da997b564fee85323d17bfc
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-07-04 11:17:02 +0630

    styled main content

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -2,23 +2,24 @@ import Header from "./components/Header"
 import data from "./data"
 import Cards from "./components/Main"
 
+
 export default function App(){
 
   const card = data.map(item=>{
-    return(
-        <Cards
-        key={item.id}
-        {...item}
-        />
-    )
+   return  (
+   <Cards
+    key={item.id}
+    {...item}
+    />
+  )
   })
-
+ 
   return (
-        <>
-        <Header/>
-       <main>
-        {card}
-       </main>
-        </>
-    )
+     <>
+     <Header/>
+     <main>
+      {card}
+     </main>
+     </>
+  )
 }
--- travel-journal/src/components/Main.jsx
+++ travel-journal/src/components/Main.jsx
@@ -3,9 +3,9 @@ import marker from "../assets/marker.png"
 export default function Cards(props){
     return (
             <section className="card">
-                <img src={props.img.src} alt="{props.img.alt}"/>
+                <img src={props.img.src} alt="{props.img.alt}" className="place-img"/>
                 <div className="about-card">
-                <img src={marker} alt="marker logo"/>
+                <img src={marker} alt="marker logo" className="marker"/>
                 <span>{props.country}</span>
                 <a href={props.googleMapsLink}>View on Google Maps</a>
                 <h2>{props.title}</h2>
